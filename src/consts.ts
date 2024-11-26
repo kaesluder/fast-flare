a61cbf424f83c3fd4b081c61b4ca548eed148f67
Kae Sluder <kaesluder@fastmail.com>
2024-11-25 23:56:32 -0500

    Conversion of existing blog entries and styles to tailwind.css

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
+export const SITE_TITLE = 'Kae Job Sluder';
 export const SITE_DESCRIPTION = 'Welcome to my website!';
