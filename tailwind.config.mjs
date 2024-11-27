fabd3a1d84491da88545288dc441380729ba6f25
Kae Sluder <kaesluder@fastmail.com>
2024-11-26 21:52:07 -0500

    conversion edits, resume, layout

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,27 +4,6 @@ import typography from '@tailwindcss/typography';
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,ts,tsx,md,mdx}'],
   theme: {
-    extend: {
-      typography: {
-        DEFAULT: {
-          css: {
-            color: 'rgb(253 244 191)', // Matches `text-amber-50`
-            a: {
-              color: 'rgb(253 244 191)', // Ensure links match
-              '&:hover': {
-                color: 'rgb(252 211 77)', // Lighter amber on hover
-              },
-            },
-            strong: {
-              color: 'rgb(253 244 191)', // Bold text matches
-            },
-            blockquote: {
-              color: 'rgb(253 244 191)',
-              borderLeftColor: 'rgb(252 211 77)', // Optional customization for blockquotes
-            },
-          },
-        },
-      },
-    },
+    extend: {},
   },plugins: [typography],
 }
