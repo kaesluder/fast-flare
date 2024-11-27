4717281cfb79c30732f3c0ea7864468091021afc
Kae Sluder <kaesluder@fastmail.com>
2024-11-26 23:54:28 -0500

    resume rename and edits

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,5 +9,9 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://example.com',
-    integrations: [mdx(), sitemap(), tailwind()],
+    integrations: [
+      mdx(), 
+      sitemap(), 
+      tailwind()
+    ],
 });
